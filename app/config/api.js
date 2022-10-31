2aaa613311acde0ade747a22649efc3211376b9b
caizhicheng01 <caizhicheng01@baidu.com>
2022-10-31 11:18:36 +0800

    app.js

--- app/config/api.js
+++ app/config/api.js
@@ -1,7 +1,5 @@
 const APIConfig = {
-    baseUrl: 'http://182.92.3.169/homemaking',
-    iCode: 'wxfca03cb8f6378647',
-    orderNo: '1234567890123456'
+    baseUrl: 'https://www.cjmwjj.com/homemaking'
 };
 
 
